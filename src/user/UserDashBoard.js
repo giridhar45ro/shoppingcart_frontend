1ad633510f8dd04e009e18bdb510019bf98a7d46
Giri Dhar <giridharreddy1000@gmail.com>
2021-05-20 11:29:34 +0530

    Front End

---
+++ src/user/UserDashBoard.js
@@ -0,0 +1,35 @@
+import React from "react";
+import Base from "../core/Base";
+import { isAutheticated } from "../auth/helper/index";
+
+const UserDash = () => {
+  const {
+    user: { name, email },
+  } = isAutheticated();
+
+  return (
+    <div className="card mb-4 bg-primary">
+      <h4 className="card-header">User Information</h4>
+      <ul className="list-group ">
+        <li className="list-group-item text-dark bg-muted">
+          <span className="badge bg-success mr-2">Name:</span> {name}
+        </li>
+        <li className="list-group-item text-dark bg-muted">
+          <span className="badge bg-success mr-2">Email:</span> {email}
+        </li>
+
+        <li className="list-group-item">
+          <span className="badge bg-danger">
+            This is users Dash board!! Area
+          </span>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+const UserDashBoard = () => {
+  return <Base title="UserDashBoard page">{UserDash()}</Base>;
+};
+
+export default UserDashBoard;
